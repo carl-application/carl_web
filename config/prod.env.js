16992d7666a6def6f5b177831f43094d38fae2b2
Ecalle Thomas <thomasecalle@hotmail.fr>
2019-06-16 22:55:31 +0200

    Fix call to key undefined + wip tests env variables

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,13 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  PLACES_API_KEY: '"AIzaSyAESY7otFyp5_STycjYwUCqkcjtqIpXhsw"',
+
+  FIREBASE_API_KEY: '"AIzaSyAESY7otFyp5_STycjYwUCqkcjtqIpXhsw"',
+  FIREBASE_AUTH_DOMAIN: '"carl-application.firebaseapp.com"',
+  FIREBASE_DATABASE_URL: '"https://carl-application.firebaseio.com"',
+  FIREBASE_PROJECT_ID: '"carl-application"',
+  FIREBASE_STORAGE_BUCKET: '"carl-application.appspot.com"',
+  FIREBASE_MESSENGING_SENDER_ID: '"791112122663"',
+  FIREBASE_APP_ID: '"1:791112122663:web:0b5273aefac86a49"',
 }
